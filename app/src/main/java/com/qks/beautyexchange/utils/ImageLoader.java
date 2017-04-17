e8f4166049c70afe331beb6916166853ec721074
linzhengloser <1007687534@qq.com>
2017-04-17 09:52:14 +0800

    修改ImageLoader.

--- app/src/main/java/com/qks/beautyexchange/utils/ImageLoader.java
+++ app/src/main/java/com/qks/beautyexchange/utils/ImageLoader.java
@@ -3,6 +3,8 @@ package com.qks.beautyexchange.utils;
 import android.net.Uri;
 import android.widget.ImageView;
 
+import com.bumptech.glide.load.Transformation;
+
 /**
  * Created by admin on 2016/3/28.
  */
@@ -10,28 +12,38 @@ public interface ImageLoader {
 
     /**
      * 加载网络图片
-     *
      * @param imageView 要显示图片的ImageView
-     * @param imageUrl  图片的URL
-     * @param options   加载图片的相关参数
+     * @param imageUrl 图片的URL
+     * @param options 加载图片的相关参数
      */
     void loadImage(ImageView imageView, String imageUrl, ImageLoaderOptions options);
 
 
     /**
      * 使用默认方式加载网络图片
+     * @param imageView
+     * @param imageUrl
      */
     void loadImage(ImageView imageView, String imageUrl);
 
     /**
      * 通过Uri加载图片
+     * @param imageView
+     * @param imageUri
+     * @param options
      */
     void loadImage(ImageView imageView, Uri imageUri, ImageLoaderOptions options);
 
     /**
      * 使用默认的方式加载Uri图片
+     * @param imageView
+     * @param imageUri
      */
     void loadImage(ImageView imageView, Uri imageUri);
+
+    void loadImage2SimpleTarget(ImageView imageView, String imageUrl, ImageLoaderOptions options);
+
+    void loadImage2SimpleTarget(ImageView imageView, String imageUrl);
 
     class ImageLoaderOptions {
         /**
@@ -48,6 +60,17 @@ public interface ImageLoader {
          * 显示缩略图的比例
          */
         public int loadThumbnail;
+
+        /**
+         * Glide中的 Transformation
+         */
+        public Transformation bitmapTransformation;
+
+        /**
+         * 是否需要设置ImageView 为 fitXY
+         */
+        public boolean isFitXY;
+
     }
 
 }
